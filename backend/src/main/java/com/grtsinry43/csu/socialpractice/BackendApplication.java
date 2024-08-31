ae983ecb3446002e78d817e0b5147a6c09af4d67
grtsinry43 <grtsinry43@outlook.com>
2024-08-31 23:58:32 +0800

    Initial Commit

---
+++ backend/src/main/java/com/grtsinry43/csu/socialpractice/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.grtsinry43.csu.socialpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
